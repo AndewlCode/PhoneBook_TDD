c3de59a5c775429ba1d687ed5cd9ebbcf5f0df65
Andrey Lobachev <andrey_lobachev@internet.ru>
2023-07-11 11:05:18 +0300

    add implemented

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class PhoneBookTest {
     @org.junit.jupiter.api.Test
-    public void testPhonebookAdd() {
+    public void testPhonebookAdd1() {
         // given:
         PhoneBook phoneBook = new PhoneBook();
 
@@ -10,8 +10,21 @@ public class PhoneBookTest {
         phoneBook.add("Иван", "9091234545");
 
         // then:
-        String username = phoneBook.findByNumber("9091234545");
-        Assertions.assertEquals("Иван",username);
+//        String username = phoneBook.findByNumber("9091234545");
+//        Assertions.assertEquals("Иван",username);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testPhonebookAdd2() {
+        // given:
+        PhoneBook phoneBook = new PhoneBook();
+
+        // when:
+        phoneBook.add("Иван", "9091234545");
+
+        // then:
+        String phoneNumber = phoneBook.contacts.get("Иван");
+        Assertions.assertEquals("9091234545",phoneNumber);
     }
 
 }
